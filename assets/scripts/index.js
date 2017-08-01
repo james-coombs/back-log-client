97d41726bf48234e165ce1acc13fd7bf98c22754
james-coombs <coombsj4@yahoo.com>
2017-08-01 08:11:53 -0400

    Remove well-control code

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -9,11 +9,11 @@ require('./example');
 const userAuth = require('./user-auth/events');
 const logCrud = require('./log-crud/events');
 const kCode = require('./k-code/konami');
-const appUi = require('./app-ui/well-control');
+// const appUi = require('./app-ui/well-control');
 
 $(() => {
   userAuth.userAddHandlers();
   logCrud.logAddHandlers();
   kCode.kCodeHandler();
-  appUi.wellControlHandler();
+  // appUi.wellControlHandler();
 });
