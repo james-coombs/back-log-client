3d522b313cf4bc16826e6744eaf1490019c1ec5c
james-coombs <coombsj4@yahoo.com>
2017-08-01 07:57:06 -0400

    Build

--- vendor.bundle.js
+++ vendor.bundle.js
@@ -100,7 +100,7 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	__webpack_require__(2);
-	module.exports = __webpack_require__(26);
+	module.exports = __webpack_require__(49);
 
 
 /***/ },
@@ -9935,7 +9935,7 @@
 
 /***/ },
 
-/***/ 26:
+/***/ 49:
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function(jQuery) {/*!
